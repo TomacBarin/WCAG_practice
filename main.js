f0b4e04088e44aeb198a14b5bd6f4b524e6bcd73
Tomac Barin Jansson <tomacbarin@me.com>
2025-11-19 10:26:47 +0100

    Finished.

---
+++ main.js
@@ -0,0 +1,7 @@
+const paraHtml = document.querySelector("#para");
+const btnHtml = document.querySelector("#button");
+
+btnHtml.addEventListener("click", () => {
+    paraHtml.textContent = "Men hon kan rulla som en boll! 😃";
+});
+
